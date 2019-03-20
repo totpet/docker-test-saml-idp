f7a4c0cd148bd7cdd508b21bc9f1b6f531fea537
Szabó Gyula <szabo.gyula@sztaki.mta.hu>
2019-03-20 10:19:43 +0100

    +eppn in authsource

--- config/simplesamlphp/authsources.php
+++ config/simplesamlphp/authsources.php
@@ -12,11 +12,13 @@ $config = array(
             'uid' => array('1'),
             'eduPersonAffiliation' => array('group1'),
             'email' => 'user1@'.getenv('VIRTUAL_HOST'),
+            'eduPersonPrincipalName' => 'user1@'.getenv('VIRTUAL_HOST'),
         ),
         'user2:user2pass' => array(
             'uid' => array('2'),
             'eduPersonAffiliation' => array('group2'),
             'email' => 'user2@'.getenv('VIRTUAL_HOST'),
+            'eduPersonPrincipalName' => 'user2@'.getenv('VIRTUAL_HOST'),
         ),
     ),
 
