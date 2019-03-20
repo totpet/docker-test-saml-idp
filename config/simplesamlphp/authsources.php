1e33ec6986100fb8f9357ef0eacd8caa1f5a308c
Szabó Gyula <szabo.gyula@sztaki.mta.hu>
2019-03-20 10:29:14 +0100

    +attributes

--- config/simplesamlphp/authsources.php
+++ config/simplesamlphp/authsources.php
@@ -5,20 +5,23 @@ $config = array(
     'admin' => array(
         'core:AdminPassword',
     ),
-
     'example-userpass' => array(
         'exampleauth:UserPass',
         'user1:user1pass' => array(
             'uid' => array('1'),
             'eduPersonAffiliation' => array('group1'),
-            'email' => 'user1@'.getenv('VIRTUAL_HOST'),
+            'eduPersonScopedAffiliation' => array('group1@'.getenv('VIRTUAL_HOST')),
+            'mail' => 'user1@'.getenv('VIRTUAL_HOST'),
             'eduPersonPrincipalName' => 'user1@'.getenv('VIRTUAL_HOST'),
+	    'displayName' => 'User1 Displayname',
         ),
         'user2:user2pass' => array(
             'uid' => array('2'),
             'eduPersonAffiliation' => array('group2'),
-            'email' => 'user2@'.getenv('VIRTUAL_HOST'),
+            'eduPersonScopedAffiliation' => array('group2@'.getenv('VIRTUAL_HOST')),
+            'mail' => 'user2@'.getenv('VIRTUAL_HOST'),
             'eduPersonPrincipalName' => 'user2@'.getenv('VIRTUAL_HOST'),
+	    'displayName' => 'User2 Displayname',
         ),
     ),
 
